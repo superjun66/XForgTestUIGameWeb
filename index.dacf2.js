c2d55ace2f77e76aa4a2a0ce858606fa79563d11
wujun <jun.wu@jiamiantech.com>
2025-08-01 18:21:20 +0800

    打包配置修改

--- index.dacf2.js
+++ index.dacf2.js
@@ -1,4 +1,4 @@
-System.register(["./application.892e3.js"], function (_export, _context) {
+System.register(["./application.92d0b.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;

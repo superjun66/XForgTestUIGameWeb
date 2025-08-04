1c7b3827661a6c9e7eb284a7ad944b8def500dde
wujun <jun.wu@jiamiantech.com>
2025-08-04 14:33:24 +0800

    2

--- cocos-js/meshopt_decoder.wasm-43855155.js
+++ cocos-js/meshopt_decoder.wasm-43855155.js
@@ -1,11 +1,2 @@
-System.register([], (function (exports, module) {
-	'use strict';
-	return {
-		execute: (function () {
-
-			var meshopt_decoder_wasm = exports('default', 'assets/meshopt_decoder.wasm-12c0a404.wasm'); /* asset-hash:bedd883e */
-
-		})
-	};
-}));
+System.register([],(function(e){"use strict";return{execute:function(){e("default","assets/meshopt_decoder.wasm-12c0a404.wasm")}}}));
 //# sourceMappingURL=meshopt_decoder.wasm-43855155.js.map
